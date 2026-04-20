4e1ea27e597be9ff9b5d60e561b8b6986a2947c1
Park Soyoung <fst704g@naver.com>
2026-04-20 11:11:33 +0900

    feat: docker build service with github actions ci/cd

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,16 +1,17 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# templates 폴더 설정 (index.html이 있는 위치)
-templates = Jinja2Templates(directory="templates")
+# 현재 파일(main.py)의 위치를 기준으로 templates 폴더 경로를 잡습니다.
+current_dir = os.path.dirname(os.path.realpath(__file__))
+templates_path = os.path.join(current_dir, "templates")
+templates = Jinja2Templates(directory=templates_path)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    # 페이지에 표시될 데이터 정의
     k8s_data = {
         "title": "Kubernetes Navigation",
         "subtitle": "컨테이너 오케스트레이션의 바다를 항해하다",
@@ -27,6 +28,6 @@ async def read_root(request: Request):
         context={"data": k8s_data}
     )
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    # if __name__ == "__main__":
+    # import uvicorn
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
